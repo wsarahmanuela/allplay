9d4beb9850b76c378baf29b26a49094f7c6b1d4b
ma-claraad <adaoclaraa@gmail.com>
2025-09-01 11:05:46 -0300

    tela inicial - feed

---
+++ front-end/feed.js
@@ -0,0 +1,24 @@
+function criarPost() {
+    let texto = document.getElementById("post-text").value.trim();
+    if (texto === "") return;
+
+    let feed = document.querySelector(".main-content");
+
+    let novoPost = document.createElement("div");
+    novoPost.className = "conteudo-post";
+
+    novoPost.innerHTML = `
+        <div class="perfil-usuario">
+            <img src="imagens/profile.picture.jpg">
+            <div>
+                <p>Usuário sem nome</p>
+                <small>Agora mesmo</small>
+            </div>
+        </div>
+        <p style="margin-top:10px; color:#333;">${texto}</p>
+    `;
+
+    feed.appendChild(novoPost);
+
+    document.getElementById("post-text").value = "";
+}
